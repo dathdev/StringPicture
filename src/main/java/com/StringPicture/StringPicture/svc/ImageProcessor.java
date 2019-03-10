11c276cd0121fc688205c708fa87e7cc444328a0
Dat Huynh <dat.huynh.5395@gmail.com>
2019-03-10 15:47:16 -0600

    crop circle image

--- src/main/java/com/StringPicture/StringPicture/svc/ImageProcessor.java
+++ src/main/java/com/StringPicture/StringPicture/svc/ImageProcessor.java
@@ -1,8 +1,12 @@
 package com.StringPicture.StringPicture.svc;
 
 import org.opencv.core.*;
+import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ImageProcessor {
     public static void main(String args[]) {
@@ -27,9 +31,22 @@ public class ImageProcessor {
             int radius = 100;
             Imgproc.circle(mask, center, radius, new Scalar(255,255,255), -1, 8, 0);
 
+            Mat masked = new Mat();
+            src.copyTo( masked, mask );
 
+            Mat thresh = new Mat();
+            Imgproc.threshold( mask, thresh, 1, 255, Imgproc.THRESH_BINARY );
 
-            Imgcodecs.imwrite("src/assets/img/result.jpg", gray);
+            List<MatOfPoint> contours = new ArrayList<>();
+            Imgproc.findContours(thresh, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+            Rect rect = Imgproc.boundingRect(contours.get(0));
+            Mat cropped = masked.submat(rect);
+
+            HighGui.imshow("Cropped circle", cropped);
+            HighGui.waitKey();
+
+            System.exit(0);
         } catch (Exception e) {
             System.out.println("error: " + e.getMessage());
         }
