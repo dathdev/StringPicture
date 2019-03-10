d688c985f6972ee35fd4d78a60d5a366f5b19be3
Dat Huynh <dat.huynh.5395@gmail.com>
2019-03-10 17:51:27 -0600

    added StorageService

---
+++ src/main/java/com/StringPicture/StringPicture/svc/Storage/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package com.StringPicture.StringPicture.svc.Storage;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
