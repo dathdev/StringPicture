acc8479e6ade7e1f00ebd6d3cada530041a13c13
Dat Huynh <dat.huynh.5395@gmail.com>
2019-03-10 19:57:31 -0600

    add controller

--- src/main/java/com/StringPicture/StringPicture/StringPictureApplication.java
+++ src/main/java/com/StringPicture/StringPicture/StringPictureApplication.java
@@ -1,13 +1,29 @@
 package com.StringPicture.StringPicture;
 
+import com.StringPicture.StringPicture.svc.Storage.StorageProperties;
+import com.StringPicture.StringPicture.svc.Storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class StringPictureApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StringPictureApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
---
+++ src/main/java/com/StringPicture/StringPicture/ws/Controllers/ImageController.java
@@ -0,0 +1,64 @@
+package com.StringPicture.StringPicture.ws.Controllers;
+
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+public class ImageController {
+    private final StorageService storageService;
+
+    @Autowired
+    public ImageController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    @GetMapping("/images")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public String listUploadedFiles() {
+        List<String> fileNames = new ArrayList<>();
+        fileNames.addAll(storageService.loadAll().map(Path::getFileName).map(Path::toString).collect(Collectors.toList()));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/json");
+
+        String json = new Gson().toJson(fileNames);
+
+        return json;
+    }
+
+    @GetMapping("/image/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok().header(
+                HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
+    @PostMapping("/image/upload")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message", "You successfully uploaded" + file.getOriginalFilename() + "!");
+
+        return "redirect:/";
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exception) {
+        return ResponseEntity.notFound().build();
+    }
+}
